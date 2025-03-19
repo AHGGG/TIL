67270c48f8f4966e4319a30c3a3d03a3140c7a09
AAGA <ironpan3160899@gmail.com>
2025-03-19 23:36:53 +0800

    update 手写transformer-2-封装.py(65%)

--- LLM/human-to-hero/手写transformer-2-封装.py
+++ LLM/human-to-hero/手写transformer-2-封装.py
@@ -1,9 +1,19 @@
+import math
 import os
 import requests
 import torch
 import torch.nn as nn
 import pandas as pd
 import matplotlib.pyplot as plt
+import tiktoken
+from torch.nn import functional as F
+
+d_model = 64  # dimention
+batch_size = 4
+context_length = 16
+num_blocks = 8
+num_heads = 4
+dropout = 0.1
 
 
 def set_cuda():
@@ -30,63 +40,134 @@ def load_data():
 
 set_cuda()
 text = load_data()
+encoding = tiktoken.get_encoding("cl100k_base")
+tokenized_text = encoding.encode(text=text)
+tokenized_text = torch.tensor(tokenized_text, dtype=torch.long)
+max_token_id = tokenized_text.max().item()
+
+# 根据8 2的比例切分训练数据集和验证数据集
+split_idx = int(len(tokenized_text) * 0.8)
+train_data = tokenized_text[:split_idx]
+val_data = tokenized_text[split_idx:]
 
 
-class EmbeddingLayer(nn.Module):
-    """复杂将用户的输入向量化，并且完成位置信息的添加"""
+class FeedForward(nn.Module):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.d_model = d_model
+        self.ffn = nn.Sequential(
+            nn.Linear(in_features=d_model, out_features=d_model * 4),
+            nn.ReLU(),
+            nn.Linear(in_features=d_model * 4, out_features=d_model),
+            nn.Dropout(dropout),
+        )
+
+    def forward(self, x):
+        return self.ffn(x)
+
+
+class Attension(nn.Module):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # 因为这里是单个头，所以这里的Wq也是根据head的个数初始化部分就行
+        self.Wq = nn.Linear(d_model, d_model // num_heads)
+        self.Wk = nn.Linear(d_model, d_model // num_heads)
+        self.Wv = nn.Linear(d_model, d_model // num_heads)
+
+    def forward(self, x):
+        # [batch_size, context_length, d_model] @ [batch_size, d_model, d_model // num_heads]
+        # ==> [batch_size, context_length, d_model // num_heads]
+        Q = x @ self.Wq
+        K = x @ self.Wk
+        V = x @ self.Wv
+
+        # [batch_size, context_length, d_model // num_heads] @ [batch_size, d_model // num_heads, context_length]
+        # ==> [batch_size, context_length, context_length]
+        attention_score = Q @ K.transpose(-2, -1)
+
+        # scale
+        attention_score /= math.sqrt(d_model // num_heads)
+
+        # mask
+        mask = torch.triu(
+            torch.ones_like(attention_score, dtype=torch.bool), diagonal=1
+        )
+        attention_score = attention_score.masked_fill(mask, -float("inf"))
+
+        # softmax, [batch_size, context_length, context_length]
+        attention_score = F.softmax(attention_score)
+
+        # [batch_size, context_length, context_length] @ batch_size, context_length, d_model // num_heads]
+        # ==> [batch_size, context_length, d_model // num_heads]
+        A = attention_score @ V
+        return A
+
+
+class MultiAttension(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.heads = nn.ModuleList([Attension() for _ in range(num_heads)])
+        self.Wo = nn.Linear(d_model, d_model)
+
+    def forward(self, x):
+        # 每个head计算出来的维度都是：[batch_size, context_length, d_model // num_heads]
+        # 所以需要在最后一个维度进行拼接，也就是dim = -1
+        # 拼接后的维度：[batch_size, context_length, d_model]
+        A = torch.cat([h(x) for h in self.heads], dim=-1)
+
+        # 最后还需要经过一个[batch_size, d_model, d_model]的权重矩阵
+        # [batch_size, context_length, d_model] @ [batch_size, d_model, d_model]
+        # ==> [batch_size, context_length, d_model]
+        # ==> 维持和我们输入一样的维度了！
+        return A @ self.Wo
+
+
+class TransformerBlock(nn.Module):
+    """这里是单个Transformer Block的逻辑，这样的Block一共会有 num_blocks 个"""
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.multi_head_attention = MultiAttension()
+        self.feed_forward = FeedForward()
+        self.layer_norm_1 = nn.LayerNorm(d_model)
+        self.layer_norm_2 = nn.LayerNorm(d_model)
 
     def forward(self, x):
-        pass
+        # 假设这里的x已经是加上位置信息后的[batch_size, context_length, d_model]
+        # 1. 先经过一个MultiHeadAttension
+        output = self.multi_head_attention(x)
+        # 2. 残差连接
+        output += x
+        # 3. 经过层归一化
+        output = self.layer_norm_1(output)
+        # 4. feed forward
+        after_feed_forward = self.feed_forward(output)
+        # 5. 残差连接, 加上feed forward之前的输入
+        output = after_feed_forward + output
+        # 6. 再经过一次层归一化
+        return self.layer_norm_2(output)
 
 
-class MultiHeadAttensionLayer(nn.Module):
-    """将输入复制出QKV，然后切分为多个head，进行一系列注意力的计算。最后拼接到一起经过Wo后变为output"""
+class TransformerLanguageModel(nn.Module):
+    """最终的多层Transformer"""
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.transformer_blocks = nn.ModuleList(
+            [TransformerBlock() for _ in range(num_blocks)]
+        )
+        # 最后将[batch_size, context_length, d_model] 转为 [batch_size, context_length, vocab_size]的矩阵
+        self.final_linear = nn.Linear(d_model, max_token_id + 1)
 
-    def forward(self, x):
-        pass
+    def forward(self, idx, targets=None):
+        """注意这里的入参不一样了
 
+        Args:
+            idx: token代表的id数组输入
+            targets: 预期输出，如果传了就是训练模式，可以根据这个target计算出loss
+        """
+        # 1. embedding
 
-class SingleTransformerBlock(nn.Module):
-    """经过前面的 MultiHeadAttension 后，残差连接+经过层归一化+FeedForwar+残差+层归一化"""
+        # 2. calc positional matrix
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-    def forward(self, x):
-        pass
-
-
-class FeedForwardLayer(nn.Module):
-    """transformer block最后需要用到FeedForward"""
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-    def forward(self, x):
-        pass
-
-
-class MultiTransformerBlock(nn.Module):
-    """循环调用前面的 SingleTransformerBlock num_blocks次"""
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-    def forward(self, x):
-        pass
-
-
-class LinearLayer(nn.Module):
-    """经过所有Transformer Block后，还需要经过一个Linear层+softmax，完成向[context_length, d_model] => [context_length, vob_size]的转化"""
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-    def forward(self, x):
-        pass
+        # TODO
